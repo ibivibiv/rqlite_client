3089c635f50d96c097ad1598bc44a15281f43ff1
Harold Hannon <haroldhannon@gmail.com>
2022-03-22 11:29:08 -0500

    Add files via upload
    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rqlite_client',
+    version='0.1',
+    packages=['rqlite_client'],
+    url='',
+    license='none',
+    author='haroldhannon',
+    author_email='hhannon@us.ibm.com',
+    description='quick rqlite wrapper for agg and cluster controllers'
+)
