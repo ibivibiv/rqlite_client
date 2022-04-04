b7af01599b4f2af4e46695c5b73d6ce630388294
Harold Hannon <haroldhannon@gmail.com>
2022-04-03 23:23:45 -0500

    Update rqlite_client.py

--- rqlite_client/rqlite_client.py
+++ rqlite_client/rqlite_client.py
@@ -52,12 +52,12 @@ def create_agg_cluster(host, port, name, scheduling_url, scheduling_topic, clust
                        reconcile):
     insert = "INSERT INTO aggregate_cluster (name, scheduling_url, scheduling_topic, cluster_strategy, storage_pool, closed, reconcile) VALUES ('{}', '{}', '{}', '{}', '{}', {}, {});".format(
         name, scheduling_url, scheduling_topic, cluster_strategy, storage_pool, closed, reconcile)
-    execute_sql(host, port, insert)
+    return execute_sql(host, port, insert)
 
 
 def get_strategy(host, port, cluster_name):
     select = "select cluster_strategy from aggregate_cluster where name = '{}'".format(cluster_name)
-    execute_query_one(host, port, select)
+    return execute_query_one(host, port, select)
 
 
 def get_cluster_resources(host, port, strategy):
@@ -68,13 +68,13 @@ def get_cluster_resources(host, port, strategy):
     else:
         return
 
-    execute_query_many(host, port, select)
+    return execute_query_many(host, port, select)
 
 
 def get_agg_cluster(host, port, cluster_name):
     select = "select * from aggregate_cluster where name = '{}'".format(cluster_name)
 
-    execute_query_many(host, port, select)
+    return execute_query_many(host, port, select)
 
 
 def get_host_resources(host, port, cluster_name, strategy):
@@ -85,61 +85,61 @@ def get_host_resources(host, port, cluster_name, strategy):
     else:
         return
 
-    execute_query_many(host, port, select)
+    return execute_query_many(host, port, select)
 
 
 def get_host(host, port, host_id):
     select = "selct * from hosts where id = {}".format(host_id)
-    execute_query_one(host, port, select)
+    return execute_query_one(host, port, select)
 
 
 def get_cpus(host, port, host_id):
     select = "select * from cpu where host_id = {}".format(host_id)
-    execute_query_many(host, port, select)
+    return execute_query_many(host, port, select)
 
 
 def get_mems(host, port, host_id):
     select = "select * from mem where host_id = {}".format(host_id)
-    execute_query_many(host, port, select)
+    return execute_query_many(host, port, select)
 
 
 def clear_cpus_for_host(host, port, host_id):
     delete = "delete from cpu where host_id = {}".format(host_id)
-    execute_sql(host, port, delete)
+    return execute_sql(host, port, delete)
 
 
 def clear_mems_for_host(host, port, host_id):
     delete = "delete from mem where host_id = {}".format(host_id)
-    execute_sql(host, port, delete)
+    return execute_sql(host, port, delete)
 
 
 def create_cpu_resource(host, port, host_id, cluster_name):
     insert = "INSERT INTO cpu (host_id, host_count, cluster_name) VALUES ({}, 1, '{}');".format(host_id, cluster_name)
-    execute_sql(host, port, insert)
+    return execute_sql(host, port, insert)
 
 
 def insert_host(host, port, host_identifier, ip, cpu, mem, disk):
     insert = "INSERT INTO hosts(host_identifier, ip, cpu, mem, disk) VALUES('{}', '{}', {}, {}, {})".format(
         host_identifier, ip, cpu, mem, disk)
-    execute_sql(host, port, insert)
+    return execute_sql(host, port, insert)
 
 
 def update_host(host, port, id, cpu_count, mem_count, capacity):
     update = "UPDATE hosts SET cpu = {}, mem = {}, disk = {} WHERE id = {}".format(
         cpu_count, mem_count, capacity, id)
-    execute_sql(host, port, update)
+    return execute_sql(host, port, update)
 
 
 def get_host(host, port, id):
     sql = "SELECT * FROM hosts WHERE id = {}".format(id)
-    execute_query_one(host, port, sql)
+    return execute_query_one(host, port, sql)
 
 
 def get_hosts(host, port):
     sql = "SELECT * FROM hosts"
-    execute_query_many(host, port, sql)
+    return execute_query_many(host, port, sql)
 
 
 def get_host_by_uuid(host, port, uuid):
     sql = "SELECT * FROM hosts WHERE  host_identifier = '{}'".format(uuid)
-    execute_query_one(host, port, sql)
+    return execute_query_one(host, port, sql)
